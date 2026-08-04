6345c1bfb0b7d96d549f9302c9451250ad094a49
Bronit <suslinv037@gmail.com>
2026-08-04 23:32:49 +0300

    update dependencies

--- src/main/java-templates/com/vivern/arpg/Reference.java
+++ src/main/java-templates/com/vivern/arpg/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
     public static final String MOD_ID = "{{ mod_id }}";
     public static final String MOD_NAME = "{{ mod_name }}";
     public static final String VERSION = "{{ mod_version }}";
-    public static final String DEPENDENCIES = "";
+    public static final String DEPENDENCIES = "required-after:configanytime;required-after:baubles;";
     public static final String MOD_PACKAGE = "{{ package }}";
     public static final String CLIENT_PROXY_CLASS = MOD_PACKAGE + ".proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = MOD_PACKAGE + ".proxy.CommonProxy";
